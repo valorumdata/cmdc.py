ecc28fd46d771b77933739d216dfd216a016e44d
Spencer Lyon <spencerlyon2@gmail.com>
2020-06-04 14:58:14 -0400

    ENH: test examples

---
+++ tests/test_examples.py
@@ -0,0 +1,18 @@
+import inspect
+import pandas as pd
+import pytest
+import cmdc.examples
+
+
+@pytest.fixture(scope="session")
+def client():
+    return cmdc.Client()
+
+
+@pytest.mark.parametrize(
+    "func", [x[1] for x in inspect.getmembers(cmdc.examples, inspect.isfunction)]
+)
+def test_examples(client, func):
+    df = func(client)
+    assert isinstance(df, pd.DataFrame)
+    assert df.shape[0] > 0
