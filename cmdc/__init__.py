d02a6e3510044c2f51d0cabecad818b56b3cf489
Spencer Lyon <spencerlyon2@gmail.com>
2020-07-31 14:43:52 -0700

    ENH: add message about depreciation of cmdc

--- cmdc/__init__.py
+++ cmdc/__init__.py
@@ -1,2 +1,14 @@
 from . import client  # noqa: F401
 from .client import Client  # noqa: F401
+import warnings
+
+
+warnings.warn(FutureWarning(
+    """
+    The `cmdc` package has been retired and renamed to `covidcountydata`
+    
+    Please use `pip install covidcountydata` to install
+    
+    After installing covidcountydata, you can continue to use the same API that existed here
+    """
+))
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="cmdc",
-    version="0.2.0",
+    version="0.3.0",
     description="Python client for accessing the COVID Modeling Data Collaborative database",
     long_description=long_description,
     long_description_content_type="text/markdown",
